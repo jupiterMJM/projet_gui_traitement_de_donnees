cc7b3e04d92cbcd301d865b488ab127d38f91f14
Maxence B. <maxence.barre59@gmail.com>
2024-11-07 15:18:46 +0100

    mise sous forme de fonction du fichier de traitement des données

--- ouverture_et_traitement_de_fichier.py
+++ ouverture_et_traitement_de_fichier.py
@@ -5,34 +5,41 @@ from annexe_gui import *
 from configuration import *
 
 
-with h.File('data.h5', "r") as f:
-    Tof1 = np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Data00']))
-    Tof2 = np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Data01']))
-    tof_axis = 10**6*np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Axis00']))
-    shutter_axis = np.squeeze(np.array(f['RawData']['Scan000']['NavAxes']['Axis01']))
+def ouverture_data_tof(path_to_data):
 
-Tof1_on = Tof1[:,np.squeeze(np.where(shutter_axis%2==1)),:]
-Tof1_off= Tof1[:,np.squeeze(np.where(shutter_axis%2==0)),:]
-Tof2_on = Tof2[:,np.squeeze(np.where(shutter_axis%2==1)),:]
-Tof2_off= Tof2[:,np.squeeze(np.where(shutter_axis%2==0)),:]
+    data_tof1 = []
+    data_tof2 = []
 
-Tof1_on_avg=np.mean(Tof1_on,axis=0)
-Tof1_off_avg=np.mean(Tof1_off,axis=0)
+    with h.File(path_to_data, "r") as f:
+        Tof1 = np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Data00']))
+        Tof2 = np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Data01']))
+        tof_axis = 10**6*np.squeeze(np.array(f['RawData']['Scan000']['Detector000']['Data1D']['CH00']['Axis00']))
+        shutter_axis = np.squeeze(np.array(f['RawData']['Scan000']['NavAxes']['Axis01']))
 
-Tof2_on_avg=np.mean(Tof2_on,axis=0)
-Tof2_off_avg=np.mean(Tof2_off,axis=0)
+    Tof1_on = Tof1[:,np.squeeze(np.where(shutter_axis%2==1)),:]
+    # Tof1_off= Tof1[:,np.squeeze(np.where(shutter_axis%2==0)),:]
+    Tof2_on = Tof2[:,np.squeeze(np.where(shutter_axis%2==1)),:]
+    # Tof2_off= Tof2[:,np.squeeze(np.where(shutter_axis%2==0)),:]
 
-Tof_considere = Tof1_on_avg[0] - np.mean(Tof1_on_avg[0][:600])
-
-alpha, t0, V0 = tof1_config
-
-energy_axis = np.flip([tof2eV(t,V0,alpha,t0) for t in tof_axis])
-signal_E = np.flip(Tof_considere)
-# signal_E = np.flip(Tof_considere)
-for i in range (len(signal_E)):
-    signal_E[i] *= np.abs(eV2TOF_Jac(energy_axis[i],alpha,V0))
+    Tof1_on_avg=np.mean(Tof1_on,axis=0)
+    # Tof1_off_avg=np.mean(Tof1_off,axis=0)
+    Tof2_on_avg=np.mean(Tof2_on,axis=0)
+    # Tof2_off_avg=np.mean(Tof2_off,axis=0)
+    return (tof_axis, Tof1_on_avg[0]), (tof_axis, Tof2_on_avg[0])
 
 
-plt.plot(energy_axis, signal_E,label='Tof1_on')
-# plt.plot(tof_axis, Tof_considere,label='Tof1_on')
-plt.show()
+def calibration_tof(data_tof, alpha, t0, V0):
+    tof_axis, tof = data_tof
+    tof_considere = tof - np.mean(tof[:600])
+    energy_axis = np.flip([tof2eV(t,V0,alpha,t0) for t in tof_axis])
+    signal_E = np.flip(tof_considere)
+    for i in range (len(signal_E)):
+        signal_E[i] *= np.abs(eV2TOF_Jac(energy_axis[i],alpha,V0))
+    return (energy_axis, signal_E)
+
+
+if __name__ == "__main__":
+    tof1_data, tof2_data = ouverture_data_tof("data.h5")
+    plt.plot(*calibration_tof(tof1_data, *tof1_config))
+    plt.plot(*calibration_tof(tof2_data, *tof2_config))
+    plt.show()
